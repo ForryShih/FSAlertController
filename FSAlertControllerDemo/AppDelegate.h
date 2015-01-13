5bb7edb1a5649d3e2c0a4ac63660c1423f617fee
ForryShih <374784680@qq.com>
2015-01-13 20:50:40 +0800

    Initial Commit

---
+++ FSAlertControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FSAlertControllerDemo
+//
+//  Created by ForryShih on 1/13/15.
+//  Copyright (c) 2015 Rampage Works. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
